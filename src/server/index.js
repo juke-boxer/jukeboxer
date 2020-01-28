e6b51a9eb237249ce69b9c6601ca11707420a85a
Noah Holt <noahryanholt@gmail.com>
2020-01-28 14:40:31 -0600

    Added user creation to API

--- src/server/index.js
+++ src/server/index.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const router = require('express-promise-router')();
 
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json());
+
 const db = require('./db');
 
 async function getUserById(req, res, next) {
@@ -11,7 +16,15 @@ async function getUserById(req, res, next) {
   return next();
 }
 
+async function createUser(req, res, next) {
+  const { username, password } = req.body;
+  await db.query('INSERT INTO users (username, password) VALUES ($1, $2)', [username, password]);
+  res.send(JSON.stringify({ message: `user ${username} was successfully created` }));
+  return next();
+}
+
 app.use(express.static('dist'));
 router.get('/api/users/getUserById/:id', getUserById);
+router.post('/api/users/createUser', createUser);
 app.use(router);
 app.listen(process.env.PORT || 8080, () => console.log(`Listening on port ${process.env.PORT || 8080}!`));
