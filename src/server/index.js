05b54f1773983b5eed0b569febdd881a63a4d177
Noah Holt <noahryanholt@gmail.com>
2020-02-20 17:33:09 -0600

    The create song command is complete

--- src/server/index.js
+++ src/server/index.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const router = require('express-promise-router')();
 const path = require('path');
 const userRoutes = require('./routes/users');
-const brainzRoutes = require('./routes/brainz');
+const songRoutes = require('./routes/songs');
 const spotifyRoutes = require('./routes/spotify');
 
 const app = express();
@@ -13,7 +13,7 @@ app.use(bodyParser.json());
 app.use(express.static('dist'));
 
 router.use('/api/users', userRoutes);
-router.use('/api/brainz', brainzRoutes);
+router.use('/api/songs', songRoutes);
 router.use('/api/spotify', spotifyRoutes);
 
 router.get('*', (req, res) => {
---
+++ src/server/routes/songs.js
@@ -0,0 +1,93 @@
+const fetch = require('node-fetch');
+const router = require('express-promise-router')();
+const { MusicBrainzApi } = require('musicbrainz-api');
+const db = require('../db');
+
+const acousticbrainz = 'https://acousticbrainz.org';
+
+const mbApi = new MusicBrainzApi({
+  botAccount: {
+    username: 'armv32020@gmail.com',
+    password: process.env.BRAINZ_PASSWORD
+  },
+  baseUrl: 'https://musicbrainz.org',
+  appName: 'jukeboxer',
+  appVersion: '0.1.0',
+  appMail: 'armv32020@gmail.com'
+});
+
+
+// If a song is not found in the songs table, fetch the song data using
+// musicbrainz and acousticbrainz
+async function createSong(req, res, next) {
+  const { artist, album, song } = req.body;
+  const MusicBrainzSearchResults = await mbApi.search('recording', {
+    artist: artist !== undefined ? artist : '',
+    release: album !== undefined ? album : '',
+    recording: song
+  });
+
+  let MusicbrainzResult;
+  try {
+    MusicbrainzResult = MusicBrainzSearchResults.recordings[0];
+  } catch {
+    MusicbrainzResult = false;
+  }
+
+  if (!MusicbrainzResult) {
+    const noMusicBrainzDataInsert = await db.query('INSERT INTO songs (title, artist, misc_data) VALUES ($1, $2, $3)', [song, artist, { album }])
+      .catch((err) => {
+        console.log(err);
+        res.send({ result: err });
+        return next();
+      });
+    res.send({ result: 'song was successfully added to the database, but was not in musicbrainz database' });
+    return next();
+  }
+
+  // We need the musicbrainz id to make calls to acousticbrainz
+  const MusicbrainzId = MusicbrainzResult.id;
+  const AcousticBrainzResult = await fetch(`${acousticbrainz}/api/v1/${MusicbrainzId}/low-level`)
+    .then(resp => resp.json())
+    .catch((err) => {
+      console.log(err);
+      res.send({ result: err });
+      return next();
+    });
+
+  const DBRow = [MusicbrainzId, song, artist, { album }];
+  if (AcousticBrainzResult.message === 'Not found') {
+    const noAcousticBrainzDataInsert = await db.query('INSERT INTO songs (mbid, title, artist, misc_data) VALUES ($1, $2, $3, $4)', DBRow)
+      .catch((err) => {
+        console.log(err);
+        res.send({ result: err });
+        return next();
+      });
+    res.send({ result: 'song was succesfully added to the database, but was not in acousticbrainz database' });
+    return next();
+  }
+
+  const { metadata, tonal, rhythm } = AcousticBrainzResult;
+  const { chords_key, chords_scale } = tonal;
+  const { tags } = metadata;
+  const { danceability } = rhythm;
+  const bpm = tags.bpm[0];
+  const date = tags.date[0];
+  const genre = tags.genre[0];
+  const misc_data = {
+    album, chords_scale, chords_key, danceability, bpm, date, genre
+  };
+  DBRow[3] = misc_data;
+  const noLoveDeepWebInsert = await db.query('INSERT INTO songs (mbid, title, artist, misc_data) VALUES ($1, $2, $3, $4)', DBRow)
+    .catch((err) => {
+      console.log(err);
+      res.send({ result: err });
+      return next();
+    });
+  res.send({ result: 'song was successfully inserted into the database' });
+  return next();
+}
+
+router.post('/createSong', createSong);
+// router.post('/findSong', findSong);
+module.exports = router;
