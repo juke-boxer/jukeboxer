b17631e3a6cae661c32e6cd6fca0457f43d45d97
Tyler <starlank@gmail.com>
2020-02-24 21:21:47 -0600

    added spotify backend functionality

--- src/server/routes/spotify.js
+++ src/server/routes/spotify.js
@@ -6,62 +6,67 @@ const db = require('../db');
 
 const redirect_uri = process.env.SPOTIFY_REDIRECT_URI;
 
+const queryString = params => Object.keys(params).map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`).join('&');
+
 router.get('/login', async (req, res) => {
+  const userID = req.query.user_id;
 
   const params = {
-    response_type: 'code',
     client_id: process.env.SPOTIFY_CLIENT_ID,
-    scope: 'user-read-private user-read-email',
-    redirect_uri
+    response_type: 'code',
+    redirect_uri,
+    scope: 'playlist-read-private user-read-private user-read-email',
+    state: userID
   };
 
-  const queryString = Object.keys(params).map((key) => {
-    return encodeURIComponent(key) + '=' + encodeURIComponent(params[key])
-  }).join('&');
-
-  console.log(redirect_uri);
-  res.redirect(`https://accounts.spotify.com/authorize?${queryString}`);
+  res.redirect(`https://accounts.spotify.com/authorize?${queryString(params)}`);
 });
 
-router.get('/callback', async (req, res) => {
-  let code = req.query.code || null
+router.get('/callback', (req, res) => {
+  const code = req.query.code || null;
+  const userID = req.query.state;
 
   const params = new URLSearchParams();
   params.append('code', code);
   params.append('redirect_uri', redirect_uri);
   params.append('grant_type', 'authorization_code');
+  params.append('client_id', process.env.SPOTIFY_CLIENT_ID);
+  params.append('client_secret', process.env.SPOTIFY_CLIENT_SECRET);
 
   fetch('https://accounts.spotify.com/api/token', {
     method: 'POST',
     body: params,
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded',
-      'Authorization': 'Basic ' + (new Buffer(
-        process.env.SPOTIFY_CLIENT_ID + ':' + process.env.SPOTIFY_CLIENT_SECRET
-      ).toString('base64'))
-    }
+    headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
   })
     .then(response => response.json())
     .then((response) => {
-
       if (response.error !== undefined) {
-        console.log(`Error: ${response.error} Desc: ${response.erorr_description}`);
-      }
-      else {
+        console.log(`Error: ${response.error} Desc: ${response.error_description}`);
+      } else {
         const accessToken = response.access_token;
         const refreshToken = response.refresh_token;
 
-        fetch('https://api.spotify.com/v1/users/starlank/playlists', {
+        // Get user playlists
+        fetch('https://api.spotify.com/v1/me/playlists', {
           headers: {
             'Authorization': `Bearer ${accessToken}`
           }
         })
-        .then(playlists => playlists.text())
-        .then((playlists) => {
-          console.log(playlists);
-        });
+          .then(playlists => playlists.json())
+          .then((playlists) => {
+            db.query('UPDATE users SET spotify_playlists=$1 WHERE userid=$2', [playlists, userID]);
+
+            //console.log(`${process.env.FRONTEND_URI}/spotify-test?${queryString(result)}`);
+            res.redirect(`${process.env.FRONTEND_URI}/spotify-test`);
+          });
       }
     });
+});
+
+router.get('/getPlaylists', async (req, res) => {
+  const userID = req.query.user_id;
+  const { rows } = await db.query('SELECT spotify_playlists FROM users WHERE userid=$1', [userID]);
+  res.json(rows[0].spotify_playlists);
 });
 
 module.exports = router;
