07494ba2787591767d74f33e1417757e61544a03
Noah Holt <noahryanholt@gmail.com>
2020-03-15 21:00:26 -0500

    I'm too tired yet again :/

--- src/server/routes/spotify.js
+++ src/server/routes/spotify.js
@@ -2,12 +2,12 @@ const router = require('express-promise-router')();
 const fetch = require('node-fetch');
 const { URLSearchParams } = require('url');
 const jwt = require('jsonwebtoken');
+const fs = require('fs');
+const qs = require('querystring');
 const db = require('../db');
 
 const redirect_uri = process.env.SPOTIFY_REDIRECT_URI;
 console.log(redirect_uri);
-const queryString = params => Object.keys(params).map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`).join('&');
-
 router.get('/login', async (req, res) => {
   const userID = req.query.user_id;
 
@@ -19,7 +19,7 @@ router.get('/login', async (req, res) => {
     state: userID
   };
 
-  res.redirect(`https://accounts.spotify.com/authorize?${queryString(params)}`);
+  res.redirect(`https://accounts.spotify.com/authorize?${qs.stringify(params)}`);
 });
 
 router.get('/callback', async (req, res, next) => {
@@ -32,49 +32,53 @@ router.get('/callback', async (req, res, next) => {
   params.append('grant_type', 'authorization_code');
   params.append('client_id', process.env.SPOTIFY_CLIENT_ID);
   params.append('client_secret', process.env.SPOTIFY_CLIENT_SECRET);
+  console.log(params);
 
-  let tokens;
-  try {
-    tokens = await fetch('https://accounts.spotify.com/api/token', {
-      method: 'POST',
-      body: params,
-      headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
-    })
-      .then(response => response.json())
-      .then((response) => {
-        if (response.error !== undefined) {
-          console.log(`Error: ${response.error} Desc: ${response.error_description}`);
-        } else {
-          return {
-            spotifyAccessToken: response.access_token,
-            spotifyRefreshtoken: response.refresh_token
-          };
-        }
-      });
-  } catch (err) {
-    console.log(err);
-    res.end('fail!');
-    return next();
-  }
+  const tokens = await fetch('https://accounts.spotify.com/api/token', {
+    method: 'POST',
+    body: params,
+    headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+  })
+    .then(response => response.json())
+    .then((response) => {
+      if (response.error !== undefined) {
+        console.log(`Error: ${response.error} Desc: ${response.error_description}`);
+        throw new Error(response.error);
+      }
+      console.log(response);
+      return {
+        spotifyAccessToken: response.access_token,
+        spotifyRefreshToken: response.refresh_token
+      };
+    }).catch((err) => {
+      console.log(err);
+      process.exit(1);
+    });
+
+  console.log('tokens v1');
   console.log(tokens);
-
   db.query('UPDATE users SET misc_data=misc_data::jsonb || $1::jsonb WHERE userid=$2',
     [JSON.stringify(tokens), userID]).then((x) => {
     console.log(x);
   }).catch((err) => {
     console.log(err);
+    process.exit(1);
   });
 
+  console.log('tokens v2');
+  console.log(tokens.spotifyAccessToken);
   fetch(`${process.env.FRONTEND_URI}/api/spotify/importPlaylists`, {
     method: 'POST',
-    body: {
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
       access_token: tokens.spotifyAccessToken,
       user_id: userID
-    }
-  }).then((x) => {
-    console.log(x);
+    })
+  }).then((resp) => {
+    console.log(resp);
   }).catch((err) => {
     console.log(err);
+    process.exit(1);
   });
 
   res.redirect(`${process.env.FRONTEND_URI}/playlists`);
@@ -82,40 +86,57 @@ router.get('/callback', async (req, res, next) => {
 
 router.post('/importPlaylists', async (req, res, next) => {
   const { access_token, user_id } = req.body;
-  let playlists;
-  try {
-    playlists = await fetch('https://api.spotify.com/v1/me/playlists', {
+  console.log(`access_token: ${access_token}`);
+  const playlistsData = await fetch('https://api.spotify.com/v1/me/playlists', {
+    headers: {
+      Authorization: `Bearer ${access_token}`
+    }
+  }).then(resp => resp.json())
+    .then((resp) => {
+      if (resp.error !== undefined) {
+        console.log(resp.error);
+        throw new Error(resp.error);
+      }
+      console.log(resp);
+      return resp.items;
+    }).catch((err) => {
+      console.log(err);
+      process.exit(1);
+    });
+
+  // fs.writeFileSync('spotify_playlists_test.json', playlistsData);
+  /*
+  const playlistsDetails = await Promise.all(playlistsData.items.map((item) => {
+    const { tracks } = item;
+    const url = tracks.href;
+    return fetch(url, {
       headers: {
         Authorization: `Bearer ${access_token}`
       }
-    }).then((resp) => {
-      resp.json();
-    });
-  } catch (err) {
-    console.log(err);
-  }
-  console.log(playlists);
-
-  let playlistsData;
-  try {
-    playlistsData = Promise.all(playlists.items.map((item) => {
-      const { tracks } = item;
-      const url = tracks.href;
-      return fetch(url, {
-        headers: {
-          Authorization: `Bearer ${access_token}`
+    }).then(resp => resp.json())
+      .then((resp) => {
+        if (resp.error) {
+          console.log(resp.error);
+          process.exit(1);
         }
-      }).then((resp) => {
-        resp.json();
+        resp.on_spotify = true;
+        return resp;
+      }).catch((err) => {
+        console.log(err);
+        process.exit(1);
       });
-    }));
-  } catch (err) {
-    console.log(err);
-    res.end('fail!');
-    return next();
-  }
+  }));
+  */
 
-  console.log(playlistsData);
+  /*
+  db.query('INSERT INTO playlists VALUES (owner, misc_data) $1 $2',
+    [user_id, playlistsDetails]).then((x) => {
+    console.log(x);
+  }).catch((err) => {
+    console.log(err);
+    process.exit(1);
+  });
+  */
   res.end('success!');
   return next();
 });
