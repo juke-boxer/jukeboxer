3a91b65e6388ce4d58998fe808f98d88d9383ff4
Noah Holt <noahryanholt@gmail.com>
2020-04-13 20:36:21 -0500

    Got list of songs

--- src/server/routes/playlists.js
+++ src/server/routes/playlists.js
@@ -1,15 +1,29 @@
 const router = require('express-promise-router')();
 const db = require('../db');
 
-router.get('/getPlaylistById/:id', async (req, res, next) => {
+router.get('/getPlaylistSongsById/:id', async (req, res, next) => {
   const { id } = req.params;
-  const { rows } = await db.query('SELECT * FROM playlists WHERE playlistid=$1', [id])
+  const query = 'SELECT * FROM songs WHERE songid IN (SELECT UNNEST(songs_list) sid FROM playlists WHERE playlistid=$1)';
+  const { rows } = await db.query(query, [id])
     .catch((err) => {
       console.log(err);
       res.send({ result: err });
       return next();
     });
-  res.json({ result: rows[0] });
+  res.json({ result: rows });
+  return next();
+});
+
+router.get('/getPlaylistById/:id', async (req, res, next) => {
+  const { id } = req.params;
+  const query = 'SELECT * FROM playlists WHERE playlistid=$1';
+  const { rows } = await db.query(query, [id])
+    .catch((err) => {
+      console.log(err);
+      res.send({ result: err });
+      return next();
+    });
+  res.json({ result: rows });
   return next();
 });
 
